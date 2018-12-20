d322b20dcdc7c02d5121c7091190ed793d415a55
Amanda Gomes <amandagomes2010@gmail.com>
2018-12-20 15:51:26 -0300

    tela de cadastro

--- app/src/main/java/com/ifpb/suaconsulta/CadastroActivity.java
+++ app/src/main/java/com/ifpb/suaconsulta/CadastroActivity.java
@@ -1,7 +1,9 @@
 package com.ifpb.suaconsulta;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -13,5 +15,10 @@ public class CadastroActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_cadastro);
+    }
+
+    public void carregarTelaLogin(View view) {
+        Intent intent = new Intent(CadastroActivity.this, LoginActivity.class);
+        startActivity(intent);
     }
 }
