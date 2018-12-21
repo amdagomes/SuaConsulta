5bb150b861b743362e773405b96f7c435f601329
Amanda Gomes <amandagomes2010@gmail.com>
2018-12-21 17:14:16 -0300

    tela minhas consultas

--- app/src/main/java/com/ifpb/suaconsulta/HomeActivity.java
+++ app/src/main/java/com/ifpb/suaconsulta/HomeActivity.java
@@ -1,7 +1,9 @@
 package com.ifpb.suaconsulta;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void carregaTelaMinhasConsultas(View view) {
+        Intent intent = new Intent(HomeActivity.this, MinhasConsultasActivity.class);
+        startActivity(intent);
     }
 }
