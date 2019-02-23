1f6ba4104e0c319519b533f920ad431ff373d73c
Amanda Gomes <amandagomes2010@gmail.com>
2019-02-23 12:40:58 -0300

    ajuste tela home

--- app/src/main/java/com/ifpb/suaconsulta/HomeActivity.java
+++ app/src/main/java/com/ifpb/suaconsulta/HomeActivity.java
@@ -15,9 +15,8 @@ public class HomeActivity extends AppCompatActivity {
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_login);
-
         setContentView(R.layout.activity_home);
+
     }
 
     public void carregaTelaMinhasConsultas(View view) {
--- app/src/main/java/com/ifpb/suaconsulta/MinhasConsultasActivity.java
+++ app/src/main/java/com/ifpb/suaconsulta/MinhasConsultasActivity.java
@@ -18,7 +18,6 @@ public class MinhasConsultasActivity extends AppCompatActivity {
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_login);
 
         setContentView(R.layout.activity_minhas_consultas);
     }
