f3b8a279092542cf696f4775b905bf94f75632a9
Amanda Gomes <amandagomes2010@gmail.com>
2019-02-25 13:23:32 -0300

    autenticação de usuario

--- app/src/main/java/com/ifpb/suaconsulta/activity/LoginActivity.java
+++ app/src/main/java/com/ifpb/suaconsulta/activity/LoginActivity.java
@@ -1,18 +1,40 @@
 package com.ifpb.suaconsulta.activity;
 
 import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.ifpb.suaconsulta.R;
+import com.ifpb.suaconsulta.helper.ConfiguracaoFirebase;
+import com.ifpb.suaconsulta.helper.VerificaCPF;
+import com.ifpb.suaconsulta.model.Usuario;
 
 public class LoginActivity extends AppCompatActivity{
+
+    private Button buttonLogin;
+    private EditText textEmail;
+    private EditText textSenha;
+    private ProgressBar progressLogin;
+
+    private Usuario usuario;
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,14 +43,65 @@ public class LoginActivity extends AppCompatActivity{
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
 
-        Button btnLogin = (Button) findViewById(R.id.button_login);
-        btnLogin.setOnClickListener(new OnClickListener() {
+        verificarUsuarioLogado();
+        inicializarComponentes();
+
+        buttonLogin.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
+                progressLogin.setVisibility(View.VISIBLE);
+
+                String email = textEmail.getText().toString();
+                String senha = textSenha.getText().toString();
+                Log.i("INFO_LOGIN: email: ", email);
+                Log.i("INFO_LOGIN: senha: ", senha);
+
+                if(email.isEmpty() || senha.isEmpty()){
+                    progressLogin.setVisibility(View.GONE);
+                    Toast.makeText(LoginActivity.this, "Preencha os campos", Toast.LENGTH_LONG).show();
+                } else{
+                    usuario = new Usuario();
+                    usuario.setEmail(email);
+                    usuario.setSenha(senha);
+                    validarLogin(usuario);
+                }
             }
         });
+    }
+
+    public void verificarUsuarioLogado(){
+        auth = ConfiguracaoFirebase.getFirebaseAuth();
+        if (auth.getCurrentUser() != null){
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            finish();
+        }
+    }
+
+    private void validarLogin(Usuario usuario) {
+        auth = ConfiguracaoFirebase.getFirebaseAuth();
+        auth.signInWithEmailAndPassword(
+                usuario.getEmail(),
+                usuario.getSenha()
+        ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    progressLogin.setVisibility(View.GONE);
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    finish();
+                } else {
+                    progressLogin.setVisibility(View.GONE);
+                    Toast.makeText(LoginActivity.this, "Erro ao tentar fazer login", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void inicializarComponentes() {
+        buttonLogin = findViewById(R.id.buttonLogin);
+        textEmail = findViewById(R.id.editLoginEmail);
+        textSenha = findViewById(R.id.editLoginSenha);
+        progressLogin = findViewById(R.id.progressLogin);
     }
 
     public void carregaTelaCadastro(View view) {
